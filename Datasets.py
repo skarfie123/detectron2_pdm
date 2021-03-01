4da6d3f3fa574b3ea6dcb078900de8157eb1f235
skarfie123 <rahulpai@rocketmail.com>
2021-03-01 16:24:34 +0000

    dataset downloader

---
+++ Datasets.py
@@ -0,0 +1,73 @@
+import os
+import subprocess
+from detectron2.data.datasets import register_coco_instances
+
+
+def downloadGround():
+    subprocess.call(
+        "cp /content/gdrive/MyDrive/Share/4YPDatasets/mask_ground.zip /content/",
+        shell=True,
+    )
+    subprocess.call(
+        "cp /content/gdrive/MyDrive/Share/4YPDatasets/ground_200_annotations.zip /content/",
+        shell=True,
+    )
+    subprocess.call("unzip mask_ground.zip > /dev/null", shell=True)
+    subprocess.call("mkdir mask_ground", shell=True)
+    subprocess.call("mv img* mask_ground/", shell=True)
+    subprocess.call("unzip ground_200_annotations.zip > /dev/null", shell=True)
+
+
+def downloadVertical():
+    subprocess.call(
+        "cp /content/gdrive/MyDrive/Share/4YPDatasets/mask_vertical.zip /content/",
+        shell=True,
+    )
+    subprocess.call(
+        "cp /content/gdrive/MyDrive/Share/4YPDatasets/vertical_200_annotations.zip /content/",
+        shell=True,
+    )
+    subprocess.call("unzip mask_vertical.zip > /dev/null", shell=True)
+    subprocess.call("mkdir mask_vertical", shell=True)
+    subprocess.call("mv img* mask_vertical/", shell=True)
+    subprocess.call("unzip vertical_200_annotations.zip > /dev/null", shell=True)
+
+
+def registerGround():
+    if not os.path.exists("/content/mask_ground/"):
+        downloadGround()
+    register_coco_instances(
+        "ground_200_train",
+        {},
+        "/content/ground_200_train.json",
+        "/content/mask_ground/",
+    )
+    register_coco_instances(
+        "ground_200_val", {}, "/content/ground_200_val.json", "/content/mask_ground/"
+    )
+    register_coco_instances(
+        "ground_200_test", {}, "/content/ground_200_test.json", "/content/mask_ground/"
+    )
+
+
+def registerVertical():
+    if not os.path.exists("/content/mask_vertical/"):
+        downloadVertical()
+    register_coco_instances(
+        "vertical_200_train",
+        {},
+        "/content/vertical_200_train.json",
+        "/content/mask_vertical/",
+    )
+    register_coco_instances(
+        "vertical_200_val",
+        {},
+        "/content/vertical_200_val.json",
+        "/content/mask_vertical/",
+    )
+    register_coco_instances(
+        "vertical_200_test",
+        {},
+        "/content/vertical_200_test.json",
+        "/content/mask_vertical/",
+    )
