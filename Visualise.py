b0993aec6bf619b1190a3b989847c7fd6d480133
skarfie123 <rahulpai@rocketmail.com>
2021-03-02 00:08:41 +0000

    skip errors

--- Visualise.py
+++ Visualise.py
@@ -19,9 +19,12 @@ def compare(
     random=None,
     scale=0.5,
     threshold=0.7,
+    outputn=None,
 ):
+    if outputn is None:
+        outputn = find_outputn()
     if cfg is None:
-        cfg = get_cfg(find_outputn())
+        cfg = get_cfg(outputn)
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold
     dataset = "vertical_200" if CustomTrainer.vNotG else "ground_200"
@@ -30,48 +33,56 @@ def compare(
     if random is not None:
         dataset_dicts = rnd.sample(dataset_dicts, random)
     count = 0
+    errors = 0
     for d in dataset_dicts:
-        if (
-            filterAnnotation is not None
-            and len(
-                list(
-                    filter(
-                        lambda a: a["category_id"] == filterAnnotation, d["annotations"]
+        try:
+            if (
+                filterAnnotation is not None
+                and len(
+                    list(
+                        filter(
+                            lambda a: a["category_id"] == filterAnnotation,
+                            d["annotations"],
+                        )
                     )
                 )
-            )
-            == 0
-        ):
-            continue
+                == 0
+            ):
+                continue
 
-        count += 1
-        im = cv2.imread(d["file_name"])
-        outputs = predictor(im)
-        v = Visualizer(
-            im[:, :, ::-1],
-            metadata=MetadataCatalog.get(dataset + set),
-            scale=scale,
-            instance_mode=ColorMode.IMAGE_BW,
-        )
-        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        if (
-            filterOutput is not None
-            and len(
-                outputs["instances"][outputs["instances"].pred_classes == filterOutput]
+            im = cv2.imread(d["file_name"])
+            outputs = predictor(im)
+            v = Visualizer(
+                im[:, :, ::-1],
+                metadata=MetadataCatalog.get(dataset + set),
+                scale=scale,
+                instance_mode=ColorMode.IMAGE_BW,
             )
-            == 0
-        ):
-            continue
+            out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+            if (
+                filterOutput is not None
+                and len(
+                    outputs["instances"][
+                        outputs["instances"].pred_classes == filterOutput
+                    ]
+                )
+                == 0
+            ):
+                continue
 
-        visualizer = Visualizer(
-            im[:, :, ::-1],
-            metadata=MetadataCatalog.get("vertical_200_train"),
-            scale=scale,
-        )
-        out2 = visualizer.draw_dataset_dict(d)
-        cv2_imshow(
-            np.concatenate(
-                (out.get_image()[:, :, ::-1], out2.get_image()[:, :, ::-1]), axis=1
+            visualizer = Visualizer(
+                im[:, :, ::-1],
+                metadata=MetadataCatalog.get("vertical_200_train"),
+                scale=scale,
             )
-        )
-    print(count)
+            out2 = visualizer.draw_dataset_dict(d)
+            cv2_imshow(
+                np.concatenate(
+                    (out.get_image()[:, :, ::-1], out2.get_image()[:, :, ::-1]), axis=1
+                )
+            )
+            count += 1
+        except:
+            print("Error:", d["file_name"])
+            errors += 1
+    print(f"Count: {count}, Errors{errors}")
