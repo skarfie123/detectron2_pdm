43573bc5ead23ea166e5bf8fe8560415f5f4c51c
skarfie123 <rahulpai@rocketmail.com>
2021-03-15 16:46:38 +0000

    cocogenerality

---
+++ cocogenerality.py
@@ -0,0 +1,49 @@
+import json
+import argparse
+import glob
+
+parser = argparse.ArgumentParser(description="Counts COCO annotations per category")
+parser.add_argument(
+    "annotations",
+    metavar="coco_annotations",
+    type=str,
+    nargs="+",
+    help="Path to COCO annotations file.",
+)
+
+args = parser.parse_args()
+
+
+def main(args):
+    if len(args.annotations) == 1:
+        args.annotations = glob.glob(args.annotations[0])
+    for ann in args.annotations:
+        with open(ann, "rt", encoding="UTF-8") as annotations:
+            coco = json.load(annotations)
+            info = coco["info"]
+            licenses = coco["licenses"]
+            images = coco["images"]
+            annotations = coco["annotations"]
+            categories = coco["categories"]
+
+            labels = {}
+            generality = {}
+
+            for c in categories:
+                labels[c["id"]] = c["name"]
+                generality[c["name"]] = {i["id"]: 0 for i in images}
+
+
+            for a in annotations:
+                generality[labels[a["category_id"]]][a["image_id"]] += 1
+
+            for k in generality:
+                generality[k] = round(len([i for i in generality[k].values() if i>0]) / len(generality[k].values()), 2)
+            
+            generality["Average"] = round(sum(generality.values()) / len(generality.values()), 2)
+
+            print(ann, generality, sep="\t")
+
+
+if __name__ == "__main__":
+    main(args)
